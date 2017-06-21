9818bada5f12f6a99e9bf23b9d9f1dd8661cf219
martajaw <m.jaworska94@gmail.com>
2017-06-21 11:53:32 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,26 @@
-#Wartości zmiennych - do zadeklarowania
-#'pdb_path' - ścieżka dostępu do katalogu z plikami pdb
-#'rv_path' - ścieżka dostępu do programu RNAView
-#'mc_path' - ścieżka dostępu do programu MC-Annotate
-#'table' - nazwa tabeli
-#'columns' - nazwy kolumn w tabeli 'table'
-#DANE DO POŁĄCZENIA Z BAZĄ:
-#'name_db'- nazwa bazy danych
-#'user_db' - nazwa użytkownika bazy danych
-#'host' - np 'localhost'
-#'passwd' - hasło dostępu
-#Przykładowe wartości:
+#Variable values - to declare:
+#'pdb_path' - path to the pdb files directory
+#'rv_path' - path to RNAView
+#'mc_path' - path to MC-Annotate
+#'table' - table name
+#'columns' - column names in table 'table'
+#Data to connect to the database:
+#'name_db'- database name
+#'user_db' - database user name
+#'host' - eg 'localhost'
+#'passwd' - password
+#Examples of values:
 
-pdb_path = '/home/marta/Licencjat/pdb_gz'
-rv_path = '/home/marta/RNAVIEW/bin/rnaview'
-mc_path = '/home/marta/Licencjat/Licencjat/MC-annotate/MC-Annotate'
-table = 'Interactions'
-columns = 'id, method, full_line, interaction, sanger, f_chain, s_chain, f_nuc, s_nuc, nr_f_nuc, nr_s_nuc'
-name_db = 'Interactions'
-user_db = 'postgres'
-host = 'localhost'
-passwd = 'minusz'
+#Before starting the program -nncomment the following lines and enter the values
+
+#pdb_path = '/home/marta/Licencjat/pdb_gz'
+#rv_path = '/home/marta/RNAVIEW/bin/rnaview'
+#mc_path = '/home/marta/Licencjat/Licencjat/MC-annotate/MC-Annotate'
+#table = 'Interactions'
+#columns = 'id, method, full_line, interaction, sanger, f_chain, s_chain, f_nuc, s_nuc, nr_f_nuc, nr_s_nuc'
+#name_db = 'Interactions'
+#user_db = 'postgres'
+#host = 'localhost'
+#passwd = '******'
 
 
