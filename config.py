a5e0cbac338df4cdde5702c08444e9db965d9f77
martajaw <m.jaworska94@gmail.com>
2020-01-28 20:21:47 +0100

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@
 
 #Before starting the program - uncomment the following lines and enter the values
 
-#pdb_path = '/home/marta/Licencjat/pdb_gz'
+#pdb_path = '/home/marta/***/pdb_gz'
 #rv_path = '/home/marta/RNAVIEW/bin/rnaview'
-#mc_path = '/home/marta/Licencjat/Licencjat/MC-annotate/MC-Annotate'
+#mc_path = '*****'
 #table = 'Interactions'
 #columns = 'id, method, full_line, interaction, sanger, f_chain, s_chain, f_nuc, s_nuc, nr_f_nuc, nr_s_nuc'
 #name_db = 'Interactions'
